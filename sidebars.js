7f08d17ff9ed0334e9b7c96efae96f645ba566f0
heywalter <guichenchen@outlook.com>
2023-03-12 12:24:38 +0800

    add custom nod

--- sidebars.js
+++ sidebars.js
@@ -154,6 +154,7 @@ const sidebars = {
                    'cloud/user-guide/data-development/monitor-task',
                    ]
              },
+             'cloud/user-guide/custom-node',
              'cloud/user-guide/operation-log',
              {
               type: 'category',

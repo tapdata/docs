01058159f8a13389f7741b3e16f68b576d35c8ce
Feynman <598186058@qq.com>
2024-12-18 15:23:34 +0800

    Update iframe.js

--- static/js/iframe.js
+++ static/js/iframe.js
@@ -2,7 +2,7 @@ if (window.self !== window.top) {
     const urlParams = new URLSearchParams(location.search)
     if (urlParams.get('from') === 'cloud') {
         let style = document.createElement('style')
-        let arr = ['div[class^="announcementBar_"]', 'div[class^="tocCollapsible_"]', '.navbar', '.theme-doc-breadcrumbs', '.theme-doc-sidebar-container', '.theme-doc-version-badge', /*'#下一步', '#下一步 + p',*/ '.theme-doc-footer', '.pagination-nav', '.footer', '._hj_feedback_container']
+        let arr = ['div[class^="announcementBar_"]', 'div[class^="tocCollapsible_"]', '.navbar', '.theme-doc-breadcrumbs', '.theme-doc-sidebar-container', '.theme-doc-version-badge', /*'#下一步', '#下一步 + p',*/ '.theme-doc-footer', '.pagination-nav', '.footer', '._hj_feedback_container', '.theme-doc-markdown > h1 ~ .tooltip']
         style.innerHTML = `
             :where(${arr.join(',')}){ display: none !important; }
         `

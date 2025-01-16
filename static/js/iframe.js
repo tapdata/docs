c54e1d3800ddb16ab7fa6b38542fc0417b5ea28a
Feynman <xufei@tapdata.io>
2025-01-16 17:30:42 +0800

    fix: Remove styles that cause the document not to display

--- static/js/iframe.js
+++ static/js/iframe.js
@@ -5,7 +5,6 @@ if (window.self !== window.top) {
         let arr = ['div[class^="announcementBar_"]', 'div[class^="tocCollapsible_"]', '.navbar', '.theme-doc-breadcrumbs', '.theme-doc-sidebar-container', '.theme-doc-version-badge', /*'#下一步', '#下一步 + p',*/ '.theme-doc-footer', '.pagination-nav', '.footer', '._hj_feedback_container']
         style.innerHTML = `
             :where(${arr.join(',')}){ display: none !important; }
-            div:has(> div[class^="announcementBar"]){ display: none !important; }
         `
         document.getElementsByTagName('head').item(0).appendChild(style)
     }

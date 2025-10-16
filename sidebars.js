334a9fb77c894844dd1c38debe55cb59d1a48fe7
Walter <guichenchen@outlook.com>
2025-10-16 22:08:46 +0800

    Add docs for integrating third-party monitoring

--- sidebars.js
+++ sidebars.js
@@ -412,6 +412,7 @@ const sidebars = {
                     ]
              },
             'administration/operation',
+            'administration/monitor-with-prometheus',
             {
               type: 'category',
               label: '故障排查',
